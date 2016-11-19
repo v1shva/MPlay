3c139a27a8fedf153c75e07a07d615533e4499cf
v1shva <vishva.sudantha@gmail.com>
2016-11-19 22:14:40 +0530

    added moods

--- CodeIgniter-3.1.2/application/views/pages/home.php
+++ CodeIgniter-3.1.2/application/views/pages/home.php
@@ -9,30 +9,18 @@
 <div class="jumbotron">
     <div class="container">
         <h1>Let us find your Music.</h1>
+        <div class="row">
+            <div class="col-md-2"><img src="<?php echo base_url("media/moods/angry.png"); ?>" width="140"border="0" class="img-circle"></div>
+            <div class="col-md-2"><img src="<?php echo base_url("media/moods/bored.png"); ?>" width="140"border="0" class="img-circle"></div>
+            <div class="col-md-2"><img src="<?php echo base_url("media/moods/confused.png"); ?>" width="140"border="0" class="img-circle"></div>
+            <div class="col-md-2"><img src="<?php echo base_url("media/moods/happy.png"); ?>" width="140"border="0" class="img-circle"></div>
+            <div class="col-md-2"><img src="<?php echo base_url("media/moods/crying.png"); ?>" width="140"border="0" class="img-circle"></div>
+            <div class="col-md-2"><img src="<?php echo base_url("media/moods/in-love.png"); ?>" width="140"border="0" class="img-circle"></div>
+        </div>
+        <br>
         <p>Community based music app. Let's share our love for music</p>
         <a href="#">Learn More</a>
-        <div class="row">
-            <img src="http://lorempixel.com/output/abstract-h-c-147-153-8.jpg" width="140" height="140" border="0" class="img-circle">
-            <img src="http://lorempixel.com/output/abstract-h-c-147-153-1.jpg" width="140" height="140" border="0" class="img-circle">
-            <img src="http://lorempixel.com/output/abstract-h-c-147-153-7.jpg" width="140" height="140" border="0" class="img-circle">
-            <img src="http://lorempixel.com/output/abstract-h-c-147-153-3.jpg" width="140" height="140" border="0" class="img-circle">
-        </div>
-        <p></p>
-        <!-- SECOND ROW -->
-        <div class="row">
-            <img src="http://lorempixel.com/output/transport-h-c-147-153-1.jpg" width="140" height="140" border="0" class="img-circle">
-            <img src="http://lorempixel.com/output/transport-h-c-147-153-7.jpg" width="140" height="140" border="0" class="img-circle">
-            <img src="http://lorempixel.com/output/transport-h-c-147-153-9.jpg" width="140" height="140" border="0" class="img-circle">
-            <img src="http://lorempixel.com/output/transport-h-c-147-153-10.jpg" width="140" height="140" border="0" class="img-circle">
-            <p></p>
-        </div>
-        <!-- THIRD ROW -->
-        <div class="row">
-            <img src="http://lorempixel.com/output/nature-h-c-147-153-8.jpg" width="140" height="140" border="0" class="img-circle">
-            <img src="http://lorempixel.com/output/nature-h-c-147-153-7.jpg" width="140" height="140" border="0" class="img-circle">
-            <img src="http://lorempixel.com/output/nature-h-c-147-153-4.jpg" width="140" height="140" border="0" class="img-circle">
-            <img src="http://lorempixel.com/output/nature-h-c-147-153-10.jpg" width="140" height="140" border="0" class="img-circle">
-        </div>
+
 
     </div>
 </div>
