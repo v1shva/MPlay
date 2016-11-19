424a731af0f996ee5edfa050648c7562059460d1
MaduraHerath <maduraherath8@gmail.com>
2016-11-19 16:56:48 +0530

    add Player to the site

--- CodeIgniter-3.1.2/application/controllers/pages.php
+++ CodeIgniter-3.1.2/application/controllers/pages.php
@@ -19,6 +19,7 @@ class Pages extends CI_Controller {
         $data['title'] = ucfirst($page); // Capitalize the first letter
         $this->load->view('templates/header', $data);
         $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/player',$data);
         $this->load->view('templates/footer', $data);
     }
 }
---
+++ CodeIgniter-3.1.2/application/views/templates/player.php
@@ -0,0 +1,66 @@
+<?php include_once('header.php') ?>
+<html>
+<head>
+	<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/app.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/glyphicons/glyphicons.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/material-design-icons/material-design-icons.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/animate.css/animate.min.css" type="text/css">
+	<link rel="stylesheet" href="assets/libs/mediaelement/build/mediaelementplayer.min.css" type="text/css">
+	<link rel="stylesheet" href="assets/libs/mediaelement/build/mep.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/font-awesome/css/font-awesome.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/font-awesome/css/font-awesome.min.css" type="text/css">
+</head>
+<div class="app-footer app-player black bg">
+	<div class="playlist mep-tracks-count-3 has-artwork is-tracklist-closed" style="width:100%">
+      	<span class="mejs-offscreen">Audio Player</span>
+      	<div id="mep_0" class="mejs-container mejs-audio" tabindex="0" role="application" aria-label="Audio Player" style="width: 100%; height: 40px;">
+      		<div class="mejs-inner">
+      			<div class="mejs-mediaelement">
+      				<audio src="http://api.soundcloud.com/tracks/269944843/stream?client_id=a10d44d431ad52868f1bce6d36f5234c"></audio>
+      			</div>
+      		<div class="mejs-layers">
+      			<div class="mejs-poster mejs-layer" style="display: none; width: 100%; height: 40px;">
+      	
+      			</div>
+      			<div class="mejs-track-actions">
+      				<button class="mejs-like-button btn btn-sm no-bg btn-icon" track-id="item-1"></button>
+      			</div>
+      			<a class="mejs-track-artwork" href="track.detail.html" style="background-image: url(&quot;images/b0.jpg&quot;);"></a>
+      			<div></div>
+                <div class="mejs-track-details">
+      			<span class="mejs-track-title">
+      				<a href="track.detail.html">Pull Up</a>
+      			</span>
+      			<span class="mejs-track-author">
+      				<a href="artist.detail.html">Summerella</a>
+      			</span>
+      		</div>
+      	<div class="mejs-track-source">
+      		<i></i>
+      	</div>
+      	<div class="mejs-overlay-error mejs-layer" style="display: none; width: 100%; height: 40px;">
+      		<div class="mejs-error">
+      			
+      		</div>
+      	</div>
+      	</div>
+      		<div class="mejs-controls">
+      			<div class="mejs-button mejs-previous-button mejs-previous">
+      				<button type="button" aria-controls="mep_0" title="Previous Track">
+      					
+      				</button>
+      			</div>
+      			<div class="mejs-button mejs-playpause-button mejs-play">
+      				<button type="button" aria-controls="mep_0" title="Play" aria-label="Play">
+      					
+      				</button>
+      			</div>
+      			<div class="mejs-button mejs-next-button mejs-next">
+      				<button type="button" aria-controls="mep_0" title="Next Track">
+      					
+      				</button>
+      			</div>
+      			<div class="mejs-time-rail" style="width: 305px;"><span class="mejs-time-total mejs-time-slider" style="width: 305px;" aria-label="Time Slider" aria-valuemin="0" aria-valuemax="254.563265" aria-valuenow="0.688887" aria-valuetext="00:00" role="slider" tabindex="0"><span class="mejs-time-buffering" style="display: none;"></span><span class="mejs-time-loaded" style="width: 239.204px;"></span><span class="mejs-time-current" style="width: 1px;"></span><span class="mejs-time-handle" style="left: -4px;"></span><span class="mejs-time-float"><span class="mejs-time-float-current">00:00</span><span class="mejs-time-float-corner"></span></span></span></div><div class="mejs-time" role="timer" aria-live="off"><span class="mejs-currenttime">00:00</span><span class="mejs-time-separator"> / </span><span class="mejs-duration">04:14</span></div><div class="mejs-button mejs-volume-button mejs-mute"><button type="button" aria-controls="mep_0" title="Mute" aria-label="Mute"></button><a href="javascript:void(0);" class="mejs-volume-slider" aria-label="volumeSlider" aria-valuemin="0" aria-valuemax="100" aria-valuenow="80" aria-valuetext="80%" role="slider" tabindex="0" style="display: none;"><span class="mejs-offscreen">Use Up/Down Arrow keys to increase or decrease volume.</span><div class="mejs-volume-total"></div><div class="mejs-volume-current" style="height: 80px; top: 20px;"></div><div class="mejs-volume-handle" style="top: 17px;"></div></a></div><div class="mejs-button mejs-repeat-button mejs-repeat"><button type="button" aria-controls="mep_0" title="Repeat"></button></div><div class="mejs-button mejs-shuffle-button mejs-repeat"><button type="button" aria-controls="mep_0" title="Shuffle"></button></div><div class="mejs-button mejs-toggle-playlist-button mejs-toggle-playlist is-closed"><button type="button" aria-controls="mep_0" title="Toggle Playlist"></button></div></div><div class="mejs-clear"></div></div></div><ol class="tracks"><li class="track"><div class="track-action"><a class="track-remove">×</a></div><div class="track-info"><span class="track-title">Pull Up</span><span class="track-author">Summerella</span></div></li><li class="track"><div class="track-action"><a class="track-remove">×</a></div><div class="track-info"><span class="track-title">Fireworks</span><span class="track-author">Kygo</span></div></li><li class="track"><div class="track-action"><a class="track-remove">×</a></div><div class="track-info"><span class="track-title">I Wanna Be In the Cavalry</span><span class="track-author">Jeremy Scott</span></div></li></ol></div>
+    </div>
+</html>
