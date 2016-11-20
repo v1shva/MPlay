ebf67406fd5b4168ce1bef7c0ddeaef1e0d6b4f3
v1shva <vishva.sudantha@gmail.com>
2016-11-21 00:01:33 +0530

    add song form

--- CodeIgniter-3.1.2/application/controllers/Pages.php
+++ CodeIgniter-3.1.2/application/controllers/Pages.php
@@ -20,6 +20,7 @@ class Pages extends CI_Controller {
         $this->load->view('templates/header', $data);
         $this->load->view('pages/'.$page, $data);
         $this->load->view('pages/emotionbar',$data);
+        $this->load->view('pages/suggestions',$data);
         $this->load->view('pages/player',$data);
         $this->load->view('templates/footer', $data);
     }
