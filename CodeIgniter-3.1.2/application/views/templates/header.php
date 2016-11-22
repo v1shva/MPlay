5eaefef212a252f807ffef0a087b2910b708f28d
MaduraHerath <maduraherath8@gmail.com>
2016-11-22 14:39:57 +0530

    add theme to a  player

---
+++ CodeIgniter-3.1.2/application/views/templates/header.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vishva
+ * Date: 11/17/2016
+ * Time: 10:40 PM
+ */ ?>
+
+<html>
+        <head>
+            <meta charset="utf-8" />
+            <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+                <title>mPlay</title>
+                <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+                <link rel="stylesheet" href="<?php echo base_url("assets/css/moodscss.css"); ?>" />
+                <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>"/>
+                <link rel="stylesheet" href="<?php echo base_url("assets/player/dist/skin/whiteskin/css/jplayer.flat.white.css"); ?>" />
+            <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+            <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+            <link rel="stylesheet" href="assets/css/ratings/star-rating.css" media="all" type="text/css">
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+            <script src="assets/js/ratings/star-rating.js" type="text/javascript"></script>
+
+
+
+        </head>
+        <body>
+
+        <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">MPlay</a>
+                        </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+                            <li><a href="#">About Us</a></li>
+
+                        </ul>
+                        <form class="navbar-form navbar-right">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        </ul>
+
+
+                </div>
+        </nav>
