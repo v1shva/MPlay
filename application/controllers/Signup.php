32343ad7787f6258a64adf9ebb9a7f79728a7e3a
Rumesha <grlakshani93@gmail.com>
2016-11-24 15:36:09 +0530

    Signup for user

--- application/controllers/Signup.php
+++ application/controllers/Signup.php
@@ -8,7 +8,7 @@ class Signup extends CI_Controller {
 
         $data['title'] = ucfirst($page); // Capitalize the first letter
         $this->load->view('templates/header', $data);
-        $this->load->view($page, $data);
+        $this->load->view('pages/Signup',$page, $data);
         $this->load->view('templates/footer', $data);
     }
 }
