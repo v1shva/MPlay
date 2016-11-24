5bce1d7fd658972f475f2d01d328c132a6a08ac8
Rumesha <grlakshani93@gmail.com>
2016-11-24 12:15:30 +0530

    Add signup form

---
+++ application/controllers/Signup.php
@@ -0,0 +1,16 @@
+<?php
+
+class Signup extends CI_Controller {
+
+    public function view($page = 'Signup')
+    {
+
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+        $this->load->view('templates/header', $data);
+        $this->load->view($page, $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
+
+?>
