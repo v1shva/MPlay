dfc1c552413d9e2b7c693eef2d46a8dd2345aea9
MaduraHerath <maduraherath8@gmail.com>
2016-11-29 11:43:27 +0530

    model user connect with the view

--- application/models/model_user.php
+++ application/models/model_user.php
@@ -3,5 +3,19 @@ class Model_user extends CI_Model {
     private $email_code;
     public function insert_user(){
         $name = $this->input->post('name');
+        $country = $this->input->post('country');
+        $dob = $this->input->('dob');
+        $mlang = $this->input->('mlang');
+        $email = $this->input->('email');
+        $password = $this->input->('password');
+
+        $sql = "INSERT INTO user (email,password,name,birthday,country,mlang,)
+        VALUES(" . $email . ",
+               " . $password  .",
+                " . $this->db->escape($name) .",
+                " . $this->db->escape($country) .",
+                " . $this->db->escape($dob) .",
+                " . $this->input->escape($mlang).")";
+
     }
 }
