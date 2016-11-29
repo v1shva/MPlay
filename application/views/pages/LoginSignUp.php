93356e2d88fdae808a7765e6247bbd42f6c8dc8c
mufa001 <mufa001@users.noreply.github.com>
2016-11-29 13:15:18 +0530

    Update LoginSignUp.php

--- application/views/pages/LoginSignUp.php
+++ application/views/pages/LoginSignUp.php
@@ -93,7 +93,7 @@ FB.getLoginStatus(function(response) {
 				  <div class="form">
 					<h2>Create an account</h2>
 					<form action="#" method="post">
-						<input type="text" name="Name" placeholder="Name" required=" ">
+					        <input type="text" name="Name" placeholder="Name" required=" ">
 						<input type="date" name="Dob" placeholder="Date Of Birth" required=" ">
 						<input type="text" name="Country" placeholder="Country" required=" ">
 						<input type="text" name="Mlanguage" placeholder="Mother Language" required=" ">
