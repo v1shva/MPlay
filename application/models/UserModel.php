98abfd7f1809eb020b4e0025be135847e4b88e1a
mufa001 <mufa001@users.noreply.github.com>
2016-12-04 00:56:35 +0530

    Update UserModel.php

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -6,9 +6,24 @@
  * Time: 1:01 PM
  */
 class UserModel extends CI_model{
+     
     public function addUserDB($details){
         $db=$this->load->database();
         $success = $this->db->insert('User',$details);
         return ($success);
     }
+    
+    function  check_login($username,$password){
+        $epassword=md5($password);
+        //echo $epassword;
+       $query_str= "SELECT id FROM  user WHERE  name = '$username' and password = '$epassword'";
+        //$result=$this->db->query($query_str,array($username,$sha1_password));
+        $result=$this->db->query($query_str);
+        if($result->num_rows()==1){
+             return $result ->row(0)->id;
+        }
+        else{
+               return false;
+        }
+}
 }
