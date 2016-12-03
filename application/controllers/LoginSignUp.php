a5b4effa18a318035bfc6c4046bde62747bd2534
mufa001 <mufa001@users.noreply.github.com>
2016-12-04 00:43:15 +0530

    Update LoginSignUp.php

--- application/controllers/LoginSignUp.php
+++ application/controllers/LoginSignUp.php
@@ -6,7 +6,7 @@ class LoginSignUp extends CI_Controller {
         parent::__construct();
         $this->load->library('session');
         $this->load->library('form_validation');
-        $this->load->model(base_url('user1_model'));
+        $this->load->model('userModel');
 
     }
 
