945625437489f8f3d5e586707ed4556130b20887
mufa001 <mufa001@users.noreply.github.com>
2016-12-04 00:54:16 +0530

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -133,4 +133,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('User1Model');
+$autoload['model'] = array('UserModel');
