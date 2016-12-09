7c335cd074320f3099dbdf74a73ee69c6112eda5
mufa001 <mufa001@users.noreply.github.com>
2016-12-09 11:11:10 +0530

    Update UserModel.php

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -14,13 +14,13 @@ class UserModel extends CI_model{
     }
     
     function  check_login($username,$password){
-        $epassword=md5($password);
+        $epassword=md5($password);//Encrypting the userpassword using md5 encryptaion
         //echo $epassword;
-       $query_str= "SELECT id FROM  user WHERE  username = '$username' and password = '$epassword'";
+       $query_str= "SELECT id FROM  user WHERE  username = '$username' and password = '$epassword'";//query to select the user who has username and password passed by post method
         //$result=$this->db->query($query_str,array($username,$sha1_password));
         $result=$this->db->query($query_str);
         if($result->num_rows()==1){
-             return $result ->row(0)->id;
+             return $result ->row(0)->id;//if there exist a recored than returning true
         }
         else{
                return false;
